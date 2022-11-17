1b4008bf296519a4f33c33158f12bb0621fc394c
Anushree Soman <anuso.deepak@gmail.com>
2022-11-17 15:15:11 -0500

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Anushree Soman on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
