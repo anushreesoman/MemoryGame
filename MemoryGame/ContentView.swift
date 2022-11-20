a064628659242069aa70fcd16932227adc485307
Anushree Soman <anuso.deepak@gmail.com>
2022-11-20 15:31:42 -0500

    Added toggle for card face on tap
    The card turns when clicked

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -9,33 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack{
-                CardView(imageName: "suit.spade.fill")
-                CardView(imageName: "suit.spade.fill")
-                CardView(imageName: "suit.diamond.fill")
-                CardView(imageName: "suit.diamond.fill")
+        VStack {
+            HStack {
+                CardView(imageName: "suit.spade.fill",
+                         isFaceUp: false)
+                CardView(imageName: "suit.spade.fill",
+                         isFaceUp: true)
+                CardView(imageName: "suit.diamond.fill",
+                         isFaceUp: true)
+                CardView(imageName: "suit.diamond.fill",
+                         isFaceUp: true)
             }
-            HStack{
-                CardView(imageName: "suit.heart.fill")
-                CardView(imageName: "suit.heart.fill")
-                CardView(imageName: "suit.club.fill")
-                CardView(imageName: "suit.club.fill")
+            HStack {
+                CardView(imageName: "suit.heart.fill",
+                         isFaceUp: true)
+                CardView(imageName: "suit.heart.fill",
+                         isFaceUp: true)
+                CardView(imageName: "suit.club.fill",
+                         isFaceUp: true)
+                CardView(imageName: "suit.club.fill",
+                         isFaceUp: false)
             }
-            HStack{
-                CardView(imageName: "star.fill")
-                CardView(imageName: "star.fill")
-                CardView(imageName: "diamond.circle.fill")
-                CardView(imageName: "diamond.circle.fill")
+            HStack {
+                CardView(imageName: "star.fill",
+                         isFaceUp: true)
+                CardView(imageName: "star.fill",
+                         isFaceUp: false)
+                CardView(imageName: "suit.diamond",
+                         isFaceUp: true)
+                CardView(imageName: "suit.diamond",
+                         isFaceUp: true)
             }
-            HStack{
-                CardView(imageName: "star.square.fill")
-                CardView(imageName: "star.square.fill")
-                CardView(imageName: "heart.circle.fill")
-                CardView(imageName: "heart.circle.fill")
+            HStack {
+                CardView(imageName: "star.square.fill",
+                         isFaceUp: true)
+                CardView(imageName: "star.square.fill",
+                         isFaceUp: true)
+                CardView(imageName: "heart.circle.fill",
+                         isFaceUp: false)
+                CardView(imageName: "heart.circle.fill",
+                         isFaceUp: true)
             }
         }
         .padding(.horizontal)
+        
     }
 }
 
@@ -47,16 +64,30 @@ struct ContentView_Previews: PreviewProvider {
 
 struct CardView: View {
     var imageName: String
+    @State var isFaceUp: Bool
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 3)
-                .foregroundColor(.orange)
-            Image(systemName: "\(imageName)").renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(20)
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill()
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 25)
+                    .stroke(lineWidth: 3)
+                    .foregroundColor(.orange)
+                Image(systemName: imageName).renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(20)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 25)
+                    .fill()
+                    .foregroundColor(.orange)
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
