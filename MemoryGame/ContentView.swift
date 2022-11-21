4e52e83590e49029ca4e5ff2f3f098924a3afd27
Anushree Soman <anuso.deepak@gmail.com>
2022-11-20 19:53:33 -0500

    Created NavigationStack for game themes
    Added home screen and navigation to themed card boards

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let transportCardFaces = ["airplane", "car", "bus", "tram", "cablecar", "ferry", "scooter", "bicycle", "sailboat", "figure.walk", "car.2", "bus.doubledecker.fill"]
+        
+        let deckCardFaces = ["suit.spade.fill", "suit.diamond.fill", "suit.heart.fill", "suit.club.fill", "suit.heart", "suit.club", "suit.diamond", "suit.spade"]
+        
+        let miscCardFaces = ["star.fill", "star.square.fill", "heart.circle.fill", "bolt.fill", "camera.on.rectangle.fill", "photo", "eye.fill"]
+    
+    var themes = ["Deck of Cards","Transport", "Miscellanous"]
     var body: some View {
-        BoardView()
+        VStack {
+            NavigationStack {
+                Form {
+                    ForEach(themes, id: \.self) { theme in
+                        NavigationLink(theme, destination: BoardView(themeCards: getThemedCards(theme: theme)))
+                    }
+                }
+                .navigationTitle("Memorize")
+            }
+        }
     }
 }
 
@@ -19,3 +35,15 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+extension ContentView {
+    func getThemedCards(theme: String) -> [String] {
+        switch theme {
+        case "Deck of Cards":
+            return deckCardFaces
+        case "Transport":
+            return transportCardFaces
+        default:
+            return miscCardFaces
+        }
+    }
+}
--- MemoryGame/Views/BoardView.swift
+++ MemoryGame/Views/BoardView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct BoardView: View {
-    let cardFaces = ["suit.spade.fill", "suit.diamond.fill", "suit.heart.fill", "suit.club.fill", "suit.heart", "suit.club", "suit.diamond", "suit.spade", "airplane", "car", "bus", "tram", "cablecar", "ferry", "scooter", "bicycle", "sailboat", "figure.walk", "car.2", "bus.doubledecker.fill", "star.fill", "star.square.fill", "heart.circle.fill", "bolt.fill", "camera.on.rectangle.fill", "photo", "eye.fill"]
+    var themeCards = [String]()
     
-    @State var cardCount = 4
+    @State var cardCount = Int.random(in: 4..<8)
     
     var body: some View {
         VStack {
             ScrollView {
                 LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem()]) {
-                    ForEach(cardFaces[0..<cardCount], id: \.self) { cardFace in
+                    ForEach(themeCards[0..<cardCount], id: \.self) { cardFace in
                         CardView(imageName: cardFace,
                                  isFaceUp: true)
                         .aspectRatio(2/3, contentMode: .fit)
@@ -24,33 +24,8 @@ struct BoardView: View {
                     .foregroundColor(.orange)
                 }
             }
-            
-            HStack {
-                remove
-                Spacer()
-                add
-            }
-            .font(.largeTitle)
         }
         .padding(.horizontal)
-    }
-    var add: some View {
-        Button {
-            if cardCount < cardFaces.count {
-                cardCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.circle")
-        }
-    }
-    var remove: some View {
-        Button {
-            if cardCount > 1 {
-                cardCount -= 1
-            }
-        } label: {
-            Image(systemName: "minus.circle")
-        }
     }
 }
 
